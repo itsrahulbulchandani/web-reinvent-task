3238066ce1bce22c89bc36ede8af23999784a542
itsrahulbulchandani <73634522+itsrahulbulchandani@users.noreply.github.com>
2024-05-14 00:33:40 +0530

    message correction

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -36,7 +36,7 @@ const Dashboard: React.FC = () => {
                 </button>
             </div>
             <Modal isOpen={showModal} onClose={handleCloseModal}>
-                <h2 className="text-xl font-bold mb-4">Registration Successful!</h2>
+                <h2 className="text-xl font-bold mb-4">Logged out!</h2>
                 <p>You have successfully Logged out.</p>
                 <button onClick={handleCloseModal} className="mt-4 px-4 py-2 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600">Close</button>
             </Modal>
